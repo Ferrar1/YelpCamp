a03c2389c401aafbd3d077449a7f4b137369a934
zhulinn <julian.lin.zhu@gmail.com>
2018-01-04 06:14:22 +0000

    comment edit delete

--- routes/comments.js
+++ routes/comments.js
@@ -35,6 +35,40 @@ router.post("/", isLoggedIn,function(req, res){
                }
            }) 
 });
+
+//edit
+router.get("/:comment_id/edit", function(req, res){
+    Comment.findById(req.params.comment_id, function(err, comment) {
+        if(err) {
+            res.redirect("back");
+        } else{
+            res.render("comments/edit", {campground_id: req.params.id, comment:comment})
+        }
+    })
+    
+});
+// update
+router.put("/:comment_id", function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment){
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/"+ req.params.id);
+        }
+    })
+})
+
+// destory
+router.delete("/:comment_id", function(req, res){
+  Comment.findByIdAndRemove(req.params.comment_id,function(err){
+      if(err){
+          res.redirect("back");
+      } else{
+          res.redirect("/campgrounds/" + req.params.id)
+      }
+  })
+})
+
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
